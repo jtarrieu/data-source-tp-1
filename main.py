c839ca594b039cbdce098f7ab21afbca295bb560
jtarrieu <jeremy.tarrieu@epfedu.fr>
2023-09-19 16:02:54 +0200

    linked to google analytics

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello from Space! 🚀"
+
+
+def hello_world():
+    prefix_google = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MZ43902EQ2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MZ43902EQ2');
+</script>"""
+
+    return prefix_google + "Hello World!"
